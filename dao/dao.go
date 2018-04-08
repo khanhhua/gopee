8358228165e41807609ccafd858c7d4fa833b35c
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-04-08 22:06:47 +0800

    Invoke function against a XLSX file

--- dao/dao.go
+++ dao/dao.go
@@ -2,11 +2,12 @@ package dao
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-const CLEARDB_DATABASE_URL = "b804b4e297b1f3:3575f604@tcp(us-cdbr-iron-east-05.cleardb.net:3306)/heroku_5b0142bce7e674e"
 
 type Client struct {
 	ID                 int64
@@ -22,10 +23,14 @@ type FuncSpec struct {
 	DropboxAccessToken string
 	FnName             string
 	XlsxFile           string
+	InputMappings      map[string]string
+	OutputMappings     map[string]string
 }
 
 // CreateClient Persists a new client
 func CreateClient(client Client) (ret Client, err error) {
+	CLEARDB_DATABASE_URL := os.Getenv("CLEARDB_DATABASE_URL")
+
 	db, dberr := sql.Open("mysql", CLEARDB_DATABASE_URL)
 	if dberr != nil {
 		err = dberr
@@ -55,6 +60,8 @@ func CreateClient(client Client) (ret Client, err error) {
 
 // GetFuncByName Gets function by name
 func GetFuncByName(clientKey string, fnName string) (ret FuncSpec, err error) {
+	CLEARDB_DATABASE_URL := os.Getenv("CLEARDB_DATABASE_URL")
+
 	db, dberr := sql.Open("mysql", CLEARDB_DATABASE_URL)
 	if dberr != nil {
 		err = dberr
@@ -63,16 +70,32 @@ func GetFuncByName(clientKey string, fnName string) (ret FuncSpec, err error) {
 	defer db.Close()
 
 	var row *sql.Row
-	row = db.QueryRow(`SELECT xlsx_file, dropbox_account_id, dropbox_access_token
+	row = db.QueryRow(`SELECT xlsx_file, dropbox_account_id, dropbox_access_token,
+		input_mappings, output_mappings
 			FROM funs
-			JOIN clients ON funs.client_key = clients.client_key
-			WHERE funs.client_key = ? AND funs.fn_name = ?`,
+			JOIN clients ON funs.client_id = clients.id
+			WHERE clients.client_key = ? AND funs.fn_name = ?`,
 		clientKey, fnName)
 
-	if err = row.Scan(&ret.XlsxFile, &ret.DropboxAccountID, &ret.DropboxAccessToken); err != nil {
+	var inputMappingsRaw, outputMappingsRaw string
+	if err = row.Scan(&ret.XlsxFile, &ret.DropboxAccountID, &ret.DropboxAccessToken,
+		&inputMappingsRaw, &outputMappingsRaw); err != nil {
+		fmt.Printf("Could not retrieve. Reason: %v", err)
 		return
 	}
 	ret.FnName = fnName
+
+	ret.InputMappings = make(map[string]string)
+	for _, pair := range strings.Split(inputMappingsRaw, ";") {
+		splat := strings.Split(pair, "=")
+		ret.InputMappings[splat[0]] = splat[1]
+	}
+
+	ret.OutputMappings = make(map[string]string)
+	for _, pair := range strings.Split(outputMappingsRaw, ";") {
+		splat := strings.Split(pair, "=")
+		ret.OutputMappings[splat[0]] = splat[1]
+	}
 
 	return
 }
--- handlers/api.go
+++ handlers/api.go
@@ -2,12 +2,20 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"io/ioutil"
+
+	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox"
+	dbxFiles "github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files"
 	"github.com/gorilla/mux"
 	"github.com/khanhhua/gopee/dao"
+
+	f1Engine "github.com/khanhhua/formula1/engine"
+	xlsx "github.com/tealeg/xlsx"
 )
-import "io/ioutil"
 
 // Call calls a precomposed function
 func Call(w http.ResponseWriter, r *http.Request) {
@@ -23,29 +31,83 @@ func Call(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var inputs map[string]interface{}
+	var rawInputs map[string]interface{}
 	if fun, err := dao.GetFuncByName(clientKey, fnName); err != nil {
+		fmt.Printf("Function '%s' not found:", fnName)
 		http.Error(w, "Bad request", 404)
 		return
 	} else if data, err := ioutil.ReadAll(r.Body); err != nil {
-		http.Error(w, "Bad request", 404)
+		http.Error(w, "Bad request", 400)
 		return
 	} else {
-		json.Unmarshal(data, &inputs)
-		if output, err := execute(fun, inputs); err != nil {
+		json.Unmarshal(data, &rawInputs)
+		inputs := make(map[string]string)
+		for key, input := range rawInputs {
+			switch input.(type) {
+			case string:
+				inputs[key] = input.(string)
+				break
+			case bool:
+				inputs[key] = strconv.FormatBool(input.(bool))
+				break
+			case float64:
+				inputs[key] = strconv.FormatFloat(input.(float64), 'E', -1, 64)
+				break
+			}
+		}
+
+		if outputs, err := execute(fun, inputs); err != nil {
+			fmt.Printf("Error during executing: %v", err)
 			http.Error(w, "Execution error", 500)
+
 			return
 		} else {
 			encoder := json.NewEncoder(w)
-			encoder.Encode(output)
+			encoder.Encode(outputs)
 		}
 	}
 }
 
-func execute(fun dao.FuncSpec, inputs map[string]interface{}) (output map[string]interface{}, err error) {
-	output = make(map[string]interface{})
-	output["A1"] = "OK"
-	output["A2"] = inputs["A2"]
+func execute(fun dao.FuncSpec, inputs map[string]string) (outputs map[string]string, err error) {
+	config := dropbox.Config{
+		Token:    fun.DropboxAccessToken,
+		LogLevel: dropbox.LogInfo, // if needed, set the desired logging level. Default is off
+	}
+	dbxClient := dbxFiles.New(config)
+	filesDownloadArgs := dbxFiles.NewDownloadArg(fun.XlsxFile)
+	if _, fileContent, dbxErr := dbxClient.Download(filesDownloadArgs); dbxErr != nil {
+		fmt.Printf("Could not download.\n %v", dbxErr)
+		err = dbxErr
+		return
+	} else if bs, fileErr := ioutil.ReadAll(fileContent); fileErr != nil {
+		fmt.Printf("Could not read.\n %v", fileErr)
+
+		err = fileErr
+		return
+	} else if xlFile, xlsxErr := xlsx.OpenBinary(bs); xlsxErr != nil {
+		err = xlsxErr
+		return
+	} else {
+		fmt.Printf("File content: %v", bs)
+
+		mappedInputs := make(map[string]string)
+		for param, address := range fun.InputMappings {
+			mappedInputs[address] = inputs[param]
+		}
+
+		mappedOutputs := make(map[string]string)
+		for _, address := range fun.OutputMappings {
+			mappedOutputs[address] = ""
+		}
+
+		engine := f1Engine.NewEngine(xlFile)
+		if err = engine.Execute(mappedInputs, &mappedOutputs); err == nil {
+			outputs = make(map[string]string)
+			for param, address := range fun.OutputMappings {
+				outputs[param] = mappedOutputs[address]
+			}
+		}
+	}
 
 	return
 }
